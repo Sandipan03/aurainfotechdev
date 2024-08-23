60f744b0f99cb8d64d19adcca3cbd7edbab8da56
Sandipan03 <sandipan2018@yahoo.com>
2024-08-23 16:55:04 +0530

    c4

--- components/First.js
+++ components/First.js
@@ -8,7 +8,9 @@ function first({className}) {
   return (
     <div className={className} style={{}}>
         <div className={styles["index-page"]}>
-        <header id="header" className={`${styles.header} d-flex align-items-center fixed-top`}>
+        
+    <main className={styles.main}>
+    <header id="header" className={`${styles.header} d-flex align-items-center fixed-top`}>
     <div className={`container-fluid container-xl position-relative d-flex align-items-center justify-content-between`}>
 
       <a href="index.html" className={`${styles.logo} d-flex align-items-center`}>
@@ -19,7 +21,7 @@ function first({className}) {
 
       <nav id="navmenu" className={styles.navmenu}>
         <ul>
-          <li><a href="#hero" className={`active`}>Home</a></li>
+          <li><a href="#hero" className={styles.active}>Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
@@ -49,8 +51,7 @@ function first({className}) {
 
     </div>
   </header>
-    <main className={styles.main}>
-  <section id="hero" className={`${styles.hero} ${styles.section} ${styles["dark-background"]}`}>
+  {/* <section id="hero" className={`${styles.hero} ${styles.section} ${styles["dark-background"]}`}>
 
 <div id="hero-carousel" data-bs-interval="5000" className={`container carousel carousel-fade`} data-bs-ride="carousel">
 
@@ -91,22 +92,8 @@ function first({className}) {
 
 </div>
 
-{/* <svg className={styles["hero-waves"]} xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
-  <defs>
-    <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
-  </defs>
-  <g className={styles.wave1}>
-    <use xlink:href="#wave-path" x="50" y="3"></use>
-  </g>
-  <g className={styles.wave2}>
-    <use xlink:href="#wave-path" x="50" y="0"></use>
-  </g>
-  <g className={styles.wave3}>
-    <use xlink:href="#wave-path" x="50" y="9"></use>
-  </g>
-</svg> */}
 
-</section>
+</section> */}
 
       <section id="about" className={`${styles.about} ${styles.section}`}>
 
